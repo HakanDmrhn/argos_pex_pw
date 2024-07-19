905020f089fce88da498096a2e722f352e4c69da
HD <hakandemirhan@web.de>
2024-07-19 11:44:33 +0200

    improve helper functions

--- tests/support/helpers.js
+++ tests/support/helpers.js
@@ -1,107 +1,74 @@
+/**
+ * Hides or modifies YouTube video elements on the page.
+ * @param {import('playwright').Page} page - The Playwright page object.
+ */
 export async function ignoreYoutube(page) {
-
-    //**************************************************************************************/
-    //***************** ON PEX YOUTUBE VIDEOS HAVE GOT 3 DIFFERENT selectors **************/
-    //************************* .r-video, .video and #video ******************************/
-
-    // selector .r-video
-    const exist_youtube_a = await page.locator('.r-video').count();
-
-    if (exist_youtube_a > 0) { // if this element exists
-
-        // console.log('Element .r-video does exist: ' + exist_youtube_a)
-        await page.evaluate(() => {
-            const youTubeVideo_a = document.querySelector('.r-video');
-            youTubeVideo_a.setAttribute('data-visual-test', 'transparent');  // you can choose between transparent, removed, blackout
-        });
+    // List of selectors to check
+    const selectors = ['.r-video', '.video', '#video'];
+  
+    for (const selector of selectors) {
+      const count = await page.locator(selector).count();
+      if (count > 0) {
+        await page.evaluate(selector => {
+          const element = document.querySelector(selector);
+          if (element) {
+            element.setAttribute('data-visual-test', 'transparent');  // You can choose between transparent, removed, blackout
+          }
+        }, selector);
+      }
     }
-    else {
-        // console.log('Element .r-video does not exist: ' + exist_youtube_a)
-    }
-
-
-    // selector .video
-    const exist_youtube_b = await page.locator('.video').count();
-
-    if (exist_youtube_b > 0) { // if this element exists
-
-        // console.log('Element .video does exist: ' + exist_youtube_b)
-        await page.evaluate(() => {
-            const youTubeVideo_b = document.querySelector('.video');
-            youTubeVideo_b.setAttribute('data-visual-test', 'transparent');  // you can choose between transparent, removed, blackout
-        });
-    }
-    else {
-        // console.log('Element .video does not exist: ' + exist_youtube_b)
-    }
-
-    // selector #video
-    const exist_youtube_c = await page.locator('#video').count();
-
-    if (exist_youtube_c > 0) { // if this element exists
-
-        // console.log('Element #video does exist: ' + exist_youtube_c)
-        await page.evaluate(() => {
-            const youTubeVideo_c = document.querySelector('#video');
-            youTubeVideo_c.setAttribute('data-visual-test', 'transparent');  // you can choose between transparent, removed, blackout
-        });
-    }
-    else {
-        // console.log('Element #video does not exist: ' + exist_youtube_c)
-    }
-}
+  }
 
 
 
-// --------------------------------------------------------------------------------------------//
-// --------------------------------------- FRESHCHAT ------------------------------------------//
-// --------------------------------------------------------------------------------------------//
-
+  /**
+ * Hides or modifies the FreshChat iframe element on the page.
+ * @param {import('playwright').Page} page - The Playwright page object.
+ */
 export async function ignoreFreshChat(page) {
-
-    const freshChat = page.locator('#fc_frame')
-    await freshChat.waitFor()  // wait for freshChat-Icon to be visible
-
+    const freshChat = page.locator('#fc_frame');
+    await freshChat.waitFor();  // Wait for FreshChat frame to be visible
+  
     await page.evaluate(() => {
-        const freshChatElement = document.querySelector('#fc_frame');
-        freshChatElement.setAttribute('data-visual-test', 'transparent'); // you can choose between transparent, removed, blackout
+      const freshChatElement = document.querySelector('#fc_frame');
+      if (freshChatElement) {
+        freshChatElement.setAttribute('data-visual-test', 'transparent');  // You can choose between transparent, removed, blackout
+      }
     });
-}
+  }
 
+  
 
-
-// --------------------------------------------------------------------------------------------//
-// ----------------------------------------- FACEBOOK ------------------------------------------//
-// --------------------------------------------------------------------------------------------//
-
+/**
+ * Hides or modifies the Facebook icon element on the page.
+ * @param {import('playwright').Page} page - The Playwright page object.
+ */
 export async function ignoreFacebook(page) {
-
-    // selector #facebook
     const facebookIcon = await page.locator('#facebook').count();
-
-    if (facebookIcon > 0) { // if this element exists
-
-        await page.evaluate(() => {
-            const facebookElement = document.querySelector('#facebook');
-            facebookElement.setAttribute('data-visual-test', 'transparent');  // you can choose between transparent, removed, blackout
-        });
+    if (facebookIcon > 0) {
+      await page.evaluate(() => {
+        const facebookElement = document.querySelector('#facebook');
+        if (facebookElement) {
+          facebookElement.setAttribute('data-visual-test', 'transparent');  // You can choose between transparent, removed, blackout
+        }
+      });
     }
-}
+  }
 
 
 
-
-
+/**
+ * Pauses all GIF animations to display the first frame.
+ * @param {import('playwright').Page} page - The Playwright page object.
+ */
 export async function ignoreGifAnimations(page) {
-
-  // Pause all GIF animations to display the first frame
-  await page.evaluate(() => {
-    // Find all img elements with a src ending in .gif
-    const gifs = document.querySelectorAll('img[src$=".gif"]');
-    gifs.forEach(gif => {
-      // Temporarily set the src to a blank image to freeze the first frame
-      const firstFrameSrc = gif.src + '#firstframe';
-      gif.src = firstFrameSrc;
+    await page.evaluate(() => {
+      // Find all img elements with a src ending in .gif
+      const gifs = document.querySelectorAll('img[src$=".gif"]');
+      gifs.forEach(gif => {
+        // Temporarily set the src to a blank image to freeze the first frame
+        const firstFrameSrc = gif.src.split('?')[0] + '?firstframe';
+        gif.src = firstFrameSrc;
+      });
     });
-  });
-}
+  }
