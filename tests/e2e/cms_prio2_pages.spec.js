1dbc3c67911967c42595df86deed55eeeebdc98d
HD <hakandemirhan@web.de>
2024-07-29 13:24:14 +0200

    revide code

--- tests/e2e/cms_prio2_pages.spec.js
+++ tests/e2e/cms_prio2_pages.spec.js
@@ -1,63 +1,53 @@
 import { argosScreenshot } from "@argos-ci/playwright";
 import { test } from '@playwright/test';
 import Freezeframe from 'freezeframe';
-import { ignoreFreshChat, ignoreYoutube } from '../support/helpers'
+import { ignoreFreshChat, ignoreYoutube } from '../support/helpers';
 
-var data = require("../fixtures/cms_prio2.json");
-var cmsPrio2_pages = data.URLS;
-let scrollToBottom = require("scroll-to-bottomjs");
+const data = require("../fixtures/cms_prio2.json");
+const cmsPrio2_pages = data.URLS;
+const scrollToBottom = require("scroll-to-bottomjs");
 
+test.describe('Integration test with visual testing - cms prio2 pages', () => {
 
-test.describe('Integration test with visual testing - cms prio2 pages', function () {
+    cmsPrio2_pages.forEach(link => {
 
-    cmsPrio2_pages.forEach(function (link) {
+        test(`load page: ${link} & take argos snapshot`, async ({ page }) => {
 
-        test('load page: ' + link + ' & take argos snapshot', async function ({ page }) {
-
-            // visit url
+            // visit URL
             await page.goto(link);
 
-            // Hier wird die Seite nach unten gescrollt um zu gewährleisten, dass alle Bilder geladen wurden
-            // await page.evaluate(() => {
-            //     window.scrollTo(0, document.body.scrollHeight);
-            // });
-            // --> dieser Schritt dauert ca 20 ms und ist wohlmöglich zu schnell (fehlende Bilder)
-            // --> stattdessen scrollToBottom verwenden (s.u.)
+            // Scroll to the bottom to ensure all images are loaded
+            await page.evaluate(scrollToBottom);
 
-            // Hier wird die Seite nach unten gescrollt um zu gewährleisten, dass alle Bilder geladen wurden
-            await page.evaluate(scrollToBottom); // --> scroll dauert ca 1,5 sec 
+            // Blackout FreshChat
+            await ignoreFreshChat(page);
+            // Blackout YouTube
+            await ignoreYoutube(page);
 
-            // blackout FreshChat
-            await ignoreFreshChat(page)
-            // blackout YouTube
-            await ignoreYoutube(page)
+            // Setup Freezeframe instance with custom selector and options
+            const animated_vs2 = new Freezeframe('#mainimage_plisseetyp_vs2', {
+                trigger: false
+            });
 
-           // setup freezeframe instance with custom selector and options
+            const animated_vs1 = new Freezeframe('#mainimage_plisseetyp_vs1', {
+                trigger: false
+            });
 
-           const animated_vs2 = new Freezeframe('#mainimage_plisseetyp_vs2', {
-             trigger: false
-           });
+            animated_vs2.stop(); // Stop animation
+            animated_vs1.stop(); // Stop animation
 
-           const animated_vs1 = new Freezeframe('#mainimage_plisseetyp_vs1', {
-            trigger: false
-          });
-  
-          animated_vs2.stop(); // stop animation
-          animated_vs1.stop(); 
+            // Wait a moment to ensure GIFs are reset to the first frame
+            await page.waitForTimeout(500);
 
-          });
-
-  // Wait a moment to ensure GIFs are reset to the first frame
-  await page.waitForTimeout(500);
-
-            // take argos screenshot
+            // Take Argos screenshot
             await argosScreenshot(page, link, {
                 viewports: [
-                    "macbook-16", // Use device preset for macbook-16 --> 1536 x 960
-                    "iphone-6" // Use device preset for iphone-6 --> 375x667
+                    "macbook-16", // Device preset for macbook-16 --> 1536 x 960
+                    "iphone-6" // Device preset for iphone-6 --> 375x667
                 ],
-                animations: "disabled"  // because of gifs on /ratgeber/plisseetyp
+                animations: "disabled" // Disable animations
             });
+
         });
-    })
-})
+    });
+});
