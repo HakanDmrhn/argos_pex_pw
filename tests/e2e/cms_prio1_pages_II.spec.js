e4219a80e71a506e35d5f300604dd6160b6c4831
HD <hakandemirhan@web.de>
2024-08-16 14:53:17 +0200

    PEX-4438 print user_agent

--- tests/e2e/cms_prio1_pages_II.spec.js
+++ tests/e2e/cms_prio1_pages_II.spec.js
@@ -13,6 +13,11 @@ test.describe('Integration test with visual testing - cms prio1 pages without fr
 
         test('load page: ' + link + ' & take argos snapshot', async function ({ page }) {
 
+            // Log the custom user agent and verify it
+            const userAgent = await page.evaluate(() => navigator.userAgent);
+            console.log(`Custom User Agent for ${link}: ${userAgent}`);
+            expect(userAgent).toContain('testing_agent_visual');
+
             // visit url
             await page.goto(link, { waitUntil: 'load' });
             await page.waitForFunction(() => document.fonts.ready);
