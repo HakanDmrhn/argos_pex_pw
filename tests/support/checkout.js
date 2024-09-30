6234ff7312b3a4428aedfb4a074db443a8b6e800
HD <hakandemirhan@web.de>
2024-09-30 15:19:37 +0200

    TAP-172 fix checkout helper

--- tests/support/checkout.js
+++ tests/support/checkout.js
@@ -164,8 +164,8 @@ exports.Checkout = class Checkout {
             ]);
 
             // Order summary
-            await ignoreFreshChat(this.page);
-            await checkButtonAvailability(this.page);
+            await this.page.waitForFunction(() => document.fonts.ready);
+
             console.log('Taking screenshot of Bestellübersicht...');
             await argosScreenshot(this.page, 'checkout - Bestellübersicht', {
                 viewports: ["macbook-16", "iphone-6"]
